946ee07665de204f50af3a930505926e62589751
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2021-08-10 15:28:11 -0500

    API RENIEC

--- assets/js/signupAPI.js
+++ assets/js/signupAPI.js
@@ -1,1 +1,21 @@
-$()
+$('#register > form').on('submit', e => {
+    e.preventDefault();
+    var lastname = $('#lastnameRegister').val();
+    var name = $('#nameRegister').val();
+    var email = $('#emailRegister').val();
+    var password1 = $('#password1Register').val();
+    var password2 = $('#password2Register').val();
+    var urlAPI = $('#register > form').attr('action');
+    
+    $.ajax({
+        url: urlAPI,
+        type: 'POST',
+        data: {
+            'lastname': lastname,
+            'name': name,
+            'email': email,
+            'password1': password1,
+            'password2': password2
+        }
+    });
+})
