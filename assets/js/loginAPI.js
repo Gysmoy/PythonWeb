36fc2446407d60a2aaecd1b4319911281f0881d5
yopirata <yoninfantearce@gmail.com>
2021-08-10 07:10:25 -0500

    gaa

---
+++ assets/js/loginAPI.js
@@ -0,0 +1,32 @@
+$('fom').submit(e => {
+    e.preventDefault();
+    var usernameLogin = $('#usernameLogin').val();
+    var passwordLogin = $('#passwordLogin').val();
+    console.log();
+   $.ajax({
+       url: '',
+       type: 'POST',
+       data: {
+           'username':usernameLogin,
+           'password':passwordLogin,
+
+       },
+       success: response => {
+           var data = JSON.parse(response);
+           $('#messageLogin').text(data.messager);
+           $('#messageLogin').fedeIn();
+           if (data.access){
+               $('#messageLogin').removeClass('danger').addClas('success');
+               location.href = './';
+           } else{
+               $('#messageLogin').removeClass('success').addClass('danger');
+           }
+       },
+       complete: function() {
+           setTimeout(function(){
+               $('#messageLogin').fedeOut();
+           }, 2500);
+       }
+   }) 
+
+})
