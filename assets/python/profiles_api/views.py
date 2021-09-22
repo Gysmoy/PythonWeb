ef1ca89700884c917b368ecb68f9793d19cf0ba9
yopirata <yoninfantearce@gmail.com>
2021-09-22 10:17:34 -0500

    yon_prueba

--- assets/python/profiles_api/views.py
+++ assets/python/profiles_api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    '''API view de prueba'''
+    def get(self, request, format=None):
+        an_apiview = ['data','de','base de  datos']
+
+        return Response({'status':'200','message':'Listado Terminado','data':an_apiview})
+
