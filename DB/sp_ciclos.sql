2a7bd2ebdcac90577ac9755470a8743e02070f66
yopirata <yoninfantearce@gmail.com>
2021-10-04 18:06:16 -0500

    yon

---
+++ DB/sp_ciclos.sql
@@ -0,0 +1,20 @@
+USE MANAGE_IT
+GO
+
+/* ================================================= */
+/* INICIO PROCEDIMIENTOS ALMACENADOS CICLOS */
+
+-- PERSONA_NATURAL -> CREATE == EXEC CICLOS_CREATE 'MENSUAL'
+CREATE PROCEDURE CICLOS_CREATE
+@id CHAR(8),
+@tipo VARCHAR(15),
+@ciclo INT
+AS BEGIN
+    DECLARE @id CHAR(8)
+    SELECT @id = 'CLC' + RIGHT('00000' + LTRIM(STR(COUNT(*) + 1)) FROM CICLOS
+    INSERT INTO CICLOS VALUES (
+        @id, @tipo, @ciclo, @estado, '1'
+    )
+END
+GO
+
