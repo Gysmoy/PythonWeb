cf1a21f70102330a6747200c1ef1d95f22885439
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2021-11-13 12:03:14 -0500

    NTS

--- assets/python/manage_it_service/query.py
+++ assets/python/manage_it_service/query.py
@@ -1,28 +1,46 @@
 from database import Database
 
-class Query(Database):
+class Query():
     status = False
     quantity = 0
     message = 'NTS'
     def __init__(self, spu, params = []):
         self.row = dict()
         self.rows = list()
+        db = Database()
         try:
-            cursor = self.connect()
+            cursor = db.connect()
             for i in range(len(params)):
                 spu += ' ?,' if (i < len(params) - 1) else ' ?'
             # Ejecutando QUERY
             if (cursor != False):
                 cursor.execute(spu, params)
                 columns = [i[0] for i in cursor.description]
-                self.rows = cursor.fetchall()
-                self.quantity = len(self.rows)
+                rows = cursor.fetchall()
+                self.quantity = len(rows)
+                if (self.quantity == 0):
+                    self.message = 'No hay datos para mostrar'
+                else:
+                    for row in rows:
+                        i = 0
+                        one = {}
+                        for x in row:
+                            one[columns[i]] = x
+                            i += 1
+                        self.rows.append(one)
+                    self.row = self.rows[0]
+                    self.message = 'Operación correcta'
             else:
                 self.message = 'No se pudo conectar a la BD'
-            
         except Exception as e:
             self.message = e
+    def getOne(self):
+        return self.row
+    def getAll(self):
+        return self.rows
 
-data = Query('USUARIOS_READ_ALL', [])
+'''data = Query('USUARIOS_READ_ALL', [])
 print(data.message)
 print(data.quantity)
+print(data.getOne())
+print(data.getAll())'''
