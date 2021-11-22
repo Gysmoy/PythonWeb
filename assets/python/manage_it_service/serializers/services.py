cd189d00f67ceae19caa72bb4025d01d4d0ad3ec
Carlos Manuel Gamboa Palomino <gamboapalominocarlosmanuel@gmail.com>
2021-11-22 17:01:55 -0500

    NTS

--- assets/python/manage_it_service/serializers/services.py
+++ assets/python/manage_it_service/serializers/services.py
@@ -1,14 +1,5 @@
 from rest_framework import serializers
 
-class HelloSerializer(serializers.Serializer):
-    '''Serializar una campo para probar nuestro APIView'''
-    name = serializers.CharField(max_length=100)
-    apellido = serializers.CharField(max_length=100)
-    telefono = serializers.CharField(max_length=100)
-    gmail = serializers.CharField(max_length=100)
-    edad = serializers.CharField(max_length=100)
-
-
 # Serializador de SERVICES
 class allService(serializers.Serializer):
     dat = serializers.CharField(max_length = 45)
